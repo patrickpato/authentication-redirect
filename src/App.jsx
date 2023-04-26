56f7d7e3ee9ff22b6092c528e366366a8db9dbcc
patrickpato <ningipatrick@gmail.com>
2023-04-26 05:25:16 +0300

    Authentication with React Tutorial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Login from './Login'
+import './App.css'
+import Dashboard from './Dashboard'
+
+function App() {
+   return (
+    <div>
+     Welcome Home
+    </div >
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './Login.jsx'
+import Dashboard from './Dashboard.jsx'
+import './index.css'
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+        <Route path="login" element={<Login />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
